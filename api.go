2c048430972b8b55329e491a847a52f1fffa2f90
Christopher Cichielo <chris.cichielo@gmail.com>
2024-08-10 16:41:48 -0400

    batman

---
+++ api.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	listenAddr string
+	store      Storage
+}
+
+func NewAPIServer(listenAddr string, store Storage) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+		store:      store,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+
+	log.Println("JSON API Server running on port:", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	switch method := r.Method; method {
+	case "GET":
+		return s.handleGetAccount(w, r)
+	case "POST":
+		return s.handleCreateAccount(w, r)
+	case "DELETE":
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("Method not allowed %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	accounts, err := s.store.GetAccounts()
+	if err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, accounts)
+}
+
+func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+	fmt.Println("Id: ", id)
+	return WriteJSON(w, http.StatusOK, &Account{})
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	request := &CreateAccountRequest{}
+
+	if err := json.NewDecoder(r.Body).Decode(request); err != nil {
+		return err
+	}
+
+	account := NewAccount(request.FirstName, request.LastName)
+	if err := s.store.CreateAccount(account); err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, account)
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+type APIError struct {
+	Error string
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
+}
---
+++ storage.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Storage interface {
+	GetAccountByID(int) (*Account, error)
+	GetAccounts() ([]*Account, error)
+	CreateAccount(*Account) error
+	DeleteAccount(int) error
+	UpdateAccount(*Account) error
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "user=postgres dbname=postgres password=gobank sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{
+		db: db,
+	}, nil
+}
+
+func (s *PostgresStore) Init() error {
+	return s.createAccountTable()
+}
+
+func (s *PostgresStore) createAccountTable() error {
+	query := `CREATE TABLE IF NOT EXISTS account (
+    id serial primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    number serial,
+    balance serial,
+    created_at timestamp
+  )`
+
+	_, err := s.db.Exec(query)
+	return err
+}
+
+func (s *PostgresStore) CreateAccount(account *Account) error {
+	query := `INSERT INTO account (first_name, last_name, number, balance, created_at)
+  VALUES ($1, $2, $3, $4, $5)`
+
+	res, err := s.db.Query(query, account.FirstName, account.LastName, account.Number, account.Balance, account.CreatedAt)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v\n", res)
+	return nil
+}
+
+func (s *PostgresStore) UpdateAccount(*Account) error {
+	return nil
+}
+
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
+}
+
+func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
+	return nil, nil
+}
+
+func (s *PostgresStore) GetAccounts() ([]*Account, error) {
+	query := `SELECT * FROM account`
+	res, err := s.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := []*Account{}
+	for res.Next() {
+		account := &Account{}
+		err := res.Scan(
+			&account.ID,
+			&account.FirstName,
+			&account.LastName,
+			&account.Number,
+			&account.Balance,
+			&account.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		accounts = append(accounts, account)
+	}
+
+	return accounts, nil
+}
